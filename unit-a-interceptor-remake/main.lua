27ab06a1ad18e9bcb005f99e4792577718640542
astrofra <astrofra@gmail.com>
2016-08-05 19:33:35 +0200

    - Simple test of the latest Plus update.

--- unit-a-interceptor-remake/main.lua
+++ unit-a-interceptor-remake/main.lua
@@ -17,10 +17,14 @@ intro_init()
 
 play_music()
 
+gsplus:SetCamera3D(0, 0, 0, 0, 0, 0, 60, 0.1, 1000)
+-- gsplus:SetBlend3D(gs.BlendOpaque)
+-- gsplus:SetBlend2D(gs.BlendAdditive)
+-- gsplus:SetDepthTest3D(false)
+
 while 1 do
-	-- gsplus:SetBlend2D(gs.BlendAdditive)
 	gsplus:Clear()
-	gsplus:Image2D(0, 0, SCR_HEIGHT / 287 / 2, "@assets/background.png", gs.Color.White)
+	-- gsplus:Image2D(0, 0, SCR_HEIGHT / 287 / 2, "@assets/background.png", gs.Color.White)
 	gsplus:Line3D(-500, -500, -500, 500, 500, 500, gs.Color.White, gs.Color.White)
 	gsplus:Line3D(500, 500, -500, -500, -500, 500, gs.Color.White, gs.Color.White)
 
