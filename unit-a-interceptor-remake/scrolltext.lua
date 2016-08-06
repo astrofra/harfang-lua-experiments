25fc9d6c2e4de2ef5ee967e366e3924ef5a361a0
astrofra <astrofra@gmail.com>
2016-08-06 13:27:45 +0200

    - Scrolltext font update.  - Scrolltext stub update.

--- unit-a-interceptor-remake/scrolltext.lua
+++ unit-a-interceptor-remake/scrolltext.lua
@@ -8,6 +8,8 @@ gl_h = 8
 scrl_x = 0
 scrl_y = 32 * ZOOM_RATIO
 
+glypth_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./0123456789:;<=>?@"
+
 function scroll_text_draw(handler)
-	handler:Blit2D(0, 0, gl_w, gl_h, scrl_x, scrl_y, gl_w * ZOOM_RATIO, gl_h * ZOOM_RATIO, "@assets/crystal.png", gs.Color.White)
+	handler:Blit2D(0, 0, gl_w, gl_h, scrl_x, scrl_y, gl_w * ZOOM_RATIO, gl_h * ZOOM_RATIO, "@assets/ua_font.png", gs.Color.White)
 end
