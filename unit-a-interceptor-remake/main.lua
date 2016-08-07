88d66319fbd46e47e91e9659bbf6abf94b59b83a
astrofra <astrofra@gmail.com>
2016-08-07 23:11:14 +0200

    - Draft Unit-A logo animation.

--- unit-a-interceptor-remake/main.lua
+++ unit-a-interceptor-remake/main.lua
@@ -16,9 +16,22 @@ function intro_init()
 	gs.MountFileDriver(gs.StdFileDriver("assets/"), "@assets/")
 end
 
+function logo_draw(dt)
+	w = 88 * ZOOM_RATIO / 2 
+	h = 72 * ZOOM_RATIO / 2
+	logo_phase = logo_phase + dt
+	w = w * math.abs(math.sin(logo_phase))
+	w = w / 2
+	w = math.floor(w)
+	gsplus:Quad2D(SCR_WIDTH/2 - w, SCR_HEIGHT/2, SCR_WIDTH/2 - w, SCR_HEIGHT/2 + h, SCR_WIDTH/2 + w, SCR_HEIGHT/2 + h, SCR_WIDTH/2 + w, SCR_HEIGHT/2, gs.Color.White, gs.Color.White, gs.Color.White, gs.Color.White, logo_tex, 0,0,1,1) 
+end
+
 intro_init()
 
 frame_count = 0
+logo_phase = 0
+
+logo_tex = gsplus:GetRenderer():LoadTexture("@assets/ua_logo.png")
 
 starfield_init()
 starfield_set_camera_velocity(gs.Vector3(0, -50, -25))
@@ -38,6 +51,7 @@ while 1 do
 	starfield_update(dt_sec:to_sec())
 	starfield_draw(gsplus)
 	scroll_text_draw(gsplus, dt_sec:to_sec())
+	logo_draw(dt_sec:to_sec())
 
 	gsplus:Flip()
 	gsplus:UpdateClock()
