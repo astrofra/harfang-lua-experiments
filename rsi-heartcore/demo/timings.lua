29a1c288559cd00a53a4b57710352f265e57d8e3
astrofra <astrofra@gmail.com>
2016-08-15 18:04:17 +0200

    - RSI HeartCore port :   * Added a table containing the timings of each FX.

---
+++ rsi-heartcore/demo/timings.lua
@@ -0,0 +1,14 @@
+source_fps = 30
+dest_fps = 60
+
+timings = {
+	fx_hello_outline=0, fx_red_sector_title=110, fx_brings_you=230, fx_heartcore_title=350, 
+	fx_buddha=470, fx_pentagon_tunnel=590, fx_heartcore_dualplayfield=710, fx_hand=830, 
+	fx_triangle_tunnel=960, fx_rsi_ring=1080, fx_rat_king=1200, fx_hexoball_grey=1320, 
+	fx_alien_greets=1560, fx_lego_credits=1890, fx_hexoball_white=2080, fx_hexoball_dark=2200, 
+	fx_grolsch=2320, fx_purple_starfield=2440, fx_gameboy_end=2715
+}
+
+function turn_frames_to_seconds(frames_table)
+	return frames_table
+end 
