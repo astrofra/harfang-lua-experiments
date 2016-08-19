8c8d4795d0c6b2566702d80799fc185f6ddd1f79
astrofra <astrofra@gmail.com>
2016-08-19 17:22:26 +0200

    - RSI HeartCore port :   * Draft RSI title fx.

--- rsi-heartcore/demo/fx_brings_you.lua
+++ rsi-heartcore/demo/fx_brings_you.lua
@@ -6,7 +6,10 @@ function start_fx_brings_you(dt)
 end
 
 function fx_brings_you(dt, demo_clock)
-	gsplus:Text2D(5, 5, "fx_brings_you")
+	fx_clock = demo_clock - get_fx_start_time('fx_brings_you')
+	fx_clock_norm = fx_clock / get_fx_end_time('fx_brings_you')
+
+	gsplus:Text2D(5, 5, "fx_brings_you" .. " " .. fx_clock)
 
 	if demo_clock > get_fx_end_time('fx_brings_you') then
 		DEMO_STATE = start_fx_hand -- start_fx_heartcore_title
